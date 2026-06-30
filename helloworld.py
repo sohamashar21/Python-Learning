00a652e9d3111e266b9f46378a4545d4fd2b7303
= <=>
2026-06-30 23:22:54 +0530

    Day 1 Python introduction files

---
+++ helloworld.py
@@ -0,0 +1,25 @@
+#INTRO
+# DAY1 - 30DaysOfPython Challange
+
+print("Hello World!")
+
+print(2 + 3)  # Addition
+print(3 - 2)  # Subtraction
+print(2 * 2)  # Multiplication
+print(3 + 2)  # Addition 
+print(3 - 2)  # Subtraction 
+print(3 * 2)  # Multiplication
+print(3 / 2)  # Divison 
+print(3 ** 2) # exponential
+print(3 // 2) # floor divison operator
+
+
+# Data types checking 
+
+print(type(10))                     # Int 
+print(type(3.14))                   # Float
+print(type(3+14j))                  # Complex
+print(type('Soham Ashar'))          # String 
+print(type([1,2,3]))                # List
+print(type({'name':'Soham Ashar'})) # Dictionary
+print(type(('soham',8.71,8.8)))     # Tuple
