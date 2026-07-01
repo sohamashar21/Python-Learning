5b8b99c7d7e6c91100d868825165ab6c076d2a8e
soham Ashar <sohamashar21@gmail.com>
2026-07-01 23:10:36 +0530

    Day 2 Python Variable and builtin functions

---
+++ 02_Day_Variables_builtin_func/variables.py
@@ -0,0 +1,27 @@
+# Variables in python
+
+first_name = '      Soham'
+last_name = 'Ashar'
+country = 'India'
+city = 'Ahmedabad'
+age = 19
+is_married = False
+skills = ['HTML','CSS','JS','React','Python']
+person_info = {
+  'first_name': 'Soham',
+  'last_name': 'Ashar',
+  'country':'India',
+  'city': 'Ahmedabad'
+}
+
+print('First name: ',first_name)
+print('First name length: ',len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ',country)
+print('City: ',city)
+print('Age: ',age)
+print('Married: ',is_married)
+print('Skills: ',skills)
+print('Person information: ',person_info)
+
