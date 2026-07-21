8ac749b4603163ba0ebc975c78ce53431792db0b
soham Ashar <sohamashar21@gmail.com>
2026-07-21 23:14:38 +0530

    06 tuples

---
+++ 06_Day_Tuples/06_tuples.py
@@ -0,0 +1,39 @@
+# syntax
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+
+# syntax
+fruits = ('banana', 'orange', 'mango', 'lemon')
+
+# syntax
+tpl = ('item1', 'item2', 'item3')
+print(len(tpl))
+
+# Syntax
+tpl = ('item1', 'item2', 'item3')
+first_item = tpl[0]
+second_item = tpl[1]
+print(first_item,second_item)
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[last_index]
+print(first_fruit,second_fruit,last_index,last_fruit)
+
+
+# Syntax
+tpl = ('item1', 'item2', 'item3','item4')
+first_item = tpl[-4]
+second_item = tpl[-3]
+print(first_item,second_item)
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+last_fruit = fruits[-1]
+print(first_fruit,second_fruit,last_fruit)
